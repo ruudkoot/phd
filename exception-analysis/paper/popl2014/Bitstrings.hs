494d7f203e12a9555978a633c1622447c55d5323
Ruud Koot <inbox@ruudkoot.nl>
2014-02-28 12:23:21 +0100

    papers

---
+++ exception-analysis/paper/popl2014/Bitstrings.hs
@@ -0,0 +1,11 @@
+module Main where
+
+type Bitstring = [Int]
+
+add :: Bitstring -> Bitstring -> Bitstring
+add []      y      = y
+add x       []     = x
+add (0:x)   (0:y)  = 0 : add x y
+add (0:x)   (1:y)  = 1 : add x y
+add (1:x)   (0:y)  = 1 : add x y
+add (1:x)   (1:y)  = 0 : add (add [1] x) y
