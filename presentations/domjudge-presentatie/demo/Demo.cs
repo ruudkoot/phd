aaa1e57fb38defbf727d608ed61d38ad5e4fb90c
Ruud Koot <inbox@ruudkoot.nl>
2015-04-22 12:05:43 +0200

    domjudge demo

---
+++ presentations/domjudge-presentatie/demo/Demo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+ 
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            
+            long t = n*(n+1)/2;
+            
+            /*
+            for (long i = u; i <= v; ++i) {
+                t += i;
+            }
+            */
+            
+            Console.WriteLine(t);           
+        }
+    }
+}
---
+++ presentations/domjudge-presentatie/demo/DemoTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+ 
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+           
+        }
+    }
+}
