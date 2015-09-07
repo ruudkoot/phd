51362c3bfcd53222a5025a791ab5394a38d81a9e
Ruud Koot <inbox@ruudkoot.nl>
2015-09-07 16:34:31 +0200

    new-lambda-ok: works, but not fully big-step yet

--- higher-ranked-exception-types/doodles/new-lambda-union/LambdaUnion.hs
+++ higher-ranked-exception-types/doodles/new-lambda-union/LambdaUnion.hs
@@ -9,9 +9,10 @@ data Name
     = Con   Idx     -- bound in "global" environment / not unifiable
     | Free  Idx     -- not bound                     / unifiable
     | Bound Idx     -- bound in "local" environment  / not unifiable
-    deriving (Eq, Read)
+    deriving (Eq, Read, Show)
 
 data Ty = Arr [Ty] Idx
+    deriving Show
 
 base :: Idx -> Ty
 base = Arr []
@@ -20,6 +21,7 @@ arity :: Ty -> Int
 arity (Arr ts _) = length ts
 
 data Tm = Tm [Ty] [(Either Name Tm, [Tm])]
+    deriving Show
 
 data Nf = Nf [Ty] [(       Name,    [Nf])]
 
@@ -39,31 +41,37 @@ checkTm cenv fenv benv (Tm tys tms)
         checkTm' benv (Right tm@(Tm tys' _), args)
             = length tys' == length args && checkTm cenv fenv benv tm
 
--- * reduction of terms to normal form
+-- * reduction of terms to normal form (big-step)
 reduce :: Tm -> Tm
-reduce (Tm tys tms) = Tm tys (map reduce' tms)
-  where reduce' :: (Either Name Tm, [Tm]) -> (Either Name Tm, [Tm])
-        reduce' (Left name, args) = (Left name, map reduce args)
-        reduce' (Right tm,  args) = 
-            let _ = applyArgs [] tm args
-             in undefined
+reduce (Tm tys tms) = Tm tys (concatMap reduce' tms)     -- FIXME: set-reduction
+  where reduce' :: (Either Name Tm, [Tm]) -> [(Either Name Tm, [Tm])]
+        reduce' (Left name, args) = [(Left name, map reduce args)]
+        reduce' (Right (Tm tys tms), args) =
+            let args' = map reduce args
+                subst = reverse (map Right args')
+             in map (applySubst' subst) tms
 
--- * apply arguments to a lamda-abstration
-applyArgs :: [Either Name Tm] -> Tm -> [Tm] -> [Tm]
-applyArgs subst (Tm tys tms) args = map (applyArgs' (reverse args ++ subst)) tms
-  where applyArgs' :: [Tm] -> (Either Name Tm, [Tm]) -> (Either Name Tm, [Tm])
-        applyArgs' subst (Left (Con idx), args')
-            = (Left (Con idx), map (applyArgs subst args'))
-        applyArgs' subst (Left (Free idx), args')
-            = (Left (Free idx), map (applyArgs subst args'))
-        applyArgs' subst (Left (Bound idx), args')
-            = (Right (subst !! idx), map (applyArgs subst args'))   -- new redex
-        applyArgs' subst (Right (Tm tys' tms'), args')
-            = let subst' = map (Left . Bound) [0 .. length tys' - 1]
-                            ++ map (increaseBinders (length tys')) subst
-               in ( Right (Tm tys' (map (applyArgs' subst') tms'))
-                  , map (\tm -> applyArgs subst' tm args) args'
-                  )
+tm1 = Tm [Arr [] 0] [(Left (Free 0), [Tm [] [(Left (Bound 0),[])]])]
+tm2 = Tm [] [(Right tm1, [Tm [] [(Left (Con 0), [])]])]
+
+-- * substitute bound variables
+type Subst = [Either Name Tm]
+
+applySubst :: Subst -> Tm -> Tm
+applySubst subst (Tm tys tms)
+    = let subst' = map (Left . Bound) [0 .. length tys - 1]
+                    ++ map (increaseBinders (length tys)) subst
+       in Tm tys $ map (applySubst' subst') tms
+
+applySubst' :: Subst -> (Either Name Tm, [Tm]) -> (Either Name Tm, [Tm])
+applySubst' subst (Left (Con idx), args)
+    = (Left (Con idx), map (applySubst subst) args)
+applySubst' subst (Left (Free idx), args)
+    = (Left (Free idx), map (applySubst subst) args)
+applySubst' subst (Left (Bound idx), args)
+    = (subst !! idx, map (applySubst subst) args)                   -- new redex
+applySubst' subst (Right tm, args)
+    = (Right (applySubst subst tm), map (applySubst subst) args)
 
 -- * increase the de Bruijn indices in a substitution environment
 increaseBinders :: Int -> Either Name Tm -> Either Name Tm
@@ -83,8 +91,5 @@ increaseBinders n = increaseBinders' 0
         increaseBindersR :: Int -> Tm -> Tm
         increaseBindersR m (Tm tys tms)
             = let m' = m + length tys
-               in Tm tys (map (increaseBinders' m' *** map (increaseBindersR m')) tms)
-
--- * convert term in normal form to normal form
-tm2nf :: Tm -> Nf
-tm2nf = undefined
+               in Tm tys $
+                    map (increaseBinders' m' *** map (increaseBindersR m')) tms
