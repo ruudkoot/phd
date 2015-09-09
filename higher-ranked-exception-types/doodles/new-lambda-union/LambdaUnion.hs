d2446981ff148d7e7275a8d4339f9517db6dfd15
Ruud Koot <inbox@ruudkoot.nl>
2015-09-09 15:13:51 +0200

    set rewriting during reduction

--- higher-ranked-exception-types/doodles/new-lambda-union/LambdaUnion.hs
+++ higher-ranked-exception-types/doodles/new-lambda-union/LambdaUnion.hs
@@ -2,6 +2,13 @@ module Main where
 
 import Control.Arrow
 import Data.Bifunctor
+import Data.Either
+import Data.List
+import GHC.Exts (sortWith)
+
+
+fromLeft :: Either a b -> a
+fromLeft (Left x) = x
 
 type Idx = Int
 
@@ -9,10 +16,10 @@ data Name
     = Con   Idx     -- bound in "global" environment / not unifiable
     | Free  Idx     -- not bound                     / unifiable
     | Bound Idx     -- bound in "local" environment  / not unifiable
-    deriving (Eq, Read, Show)
+    deriving (Eq, Ord, Read, Show)
 
 data Ty = Arr [Ty] Idx
-    deriving Show
+    deriving (Eq, Show)
 
 base :: Idx -> Ty
 base = Arr []
@@ -46,16 +53,37 @@ checkTm cenv fenv benv (Tm tys tms)
             | length tys' == length args && checkTm cenv fenv benv tm
                 = True
             | otherwise                          = error "D"
-            
+
 -- * term in normal form to normal form
 tm2nf :: Tm -> Nf
 tm2nf (Tm tys tms) = Nf tys (map tm2nf' tms)
   where tm2nf' :: (Either Name Tm, [Tm]) -> (Name, [Nf])
         tm2nf' (Left x, args) = (x, map tm2nf args)
 
+-- * rewrite "set" into normal form
+setRewrite :: [(Either Name Tm, [Tm])] -> [(Either Name Tm, [Tm])]
+setRewrite as =
+    let (ns, ts) = partition (isLeft . fst) as
+        ns'      = combineN (sortWith fst (map (fromLeft *** id) ns))
+        ts'      = if null ts then [] else error "setRewrite: found redex"
+     in map (Left *** id) ns' ++ ts'
+  where combineN :: [(Name, [Tm])] -> [(Name, [Tm])]
+        combineN []
+            = []
+        combineN [(x,xs)]
+            = [(x,xs)]
+        combineN ((x,xs):(y,ys):zzs)
+            | x == y    = combineN ((x, zipWith combineArgs xs ys) : zzs)
+            | otherwise = (x,xs) : combineN ((y,ys) : zzs)
+
+        combineArgs :: Tm -> Tm -> Tm
+        combineArgs (Tm tys1 tms1) (Tm tys2 tms2)
+            | tys1 == tys2 = Tm tys1 (setRewrite (tms1 ++ tms2))    -- recursion
+            | otherwise    = error "combineArgs"                    -- new redices?
+
 -- * reduction of terms to normal form (big-step)
-reduce :: Tm -> Tm                                      -- FIXME: Tm -> Nf?
-reduce (Tm tys tms) = Tm tys (concatMap reduce' tms)    -- FIXME: set-reduction
+reduce :: Tm -> Tm  -- FIXME: Tm -> Nf?
+reduce (Tm tys tms) = Tm tys (setRewrite (concatMap reduce' tms))
 
 reduce' :: (Either Name Tm, [Tm]) -> [(Either Name Tm, [Tm])]
 reduce' (Left name, args) = [(Left name, map reduce args)]
@@ -112,4 +140,4 @@ bound = Left . Bound
 term  = Right
 
 tm1 = Tm [base 0] [(free 0, [Tm [] [(bound 0, [])]])]
-tm2 = Tm [] [(term tm1, [Tm [] [(con 0, [])]])]
+tm2 = Tm [] [(term tm1, [Tm [] [(con 1, []),(con 0, []),(con 0,[])]])]
